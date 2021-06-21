b0270e82ceaa65a2154178ad10d1de4b4881dec4
涛 胡 <329749422@qq.com>
2021-06-21 09:38:40 +0800

    看视频 ActionFilter

---
+++ 17bangMVC/Filters/ModelErrolTransferFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _17bangMVC.Filters
+{
+    public class ModelErrolTransferFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            ModelStateDictionary modelState = ((Controller)filterContext.Controller).ModelState;
+            if (filterContext.HttpContext.Request.HttpMethod == "POST")
+            {
+                if (!modelState.IsValid)
+                {
+                    //filterContext.Controller.TempData[Keys.ErrorInModel] = modelState;
+                    filterContext.Result = new RedirectResult(filterContext.HttpContext.Request.Url.PathAndQuery);//重定向
+                }
+            }
+            else if (filterContext.HttpContext.Request.HttpMethod == "GET")
+            {
+                //ModelStateDictionary errors = filterContext.Controller.TempData[kyes.ErrorInModel] as ModelStateDictionary;
+                if (errors != null)
+                {
+                    modelState.Merge(errors);
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+        
+    }
+}
